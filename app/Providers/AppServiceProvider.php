722a02c8e61ee80ee2e18d788e535d40b6450796
Rubelkhan1997 <eng.rubelkhan797991@gmail.com>
2026-03-29 16:48:14 +0600

    Delete other module

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Modules\Booking\Models\OtaSync;
-use App\Modules\Booking\Observers\OtaSyncObserver;
 use App\Modules\FrontDesk\Models\Reservation;
 use App\Modules\FrontDesk\Observers\ReservationObserver;
 use App\Modules\Guest\Models\GuestProfile;
@@ -28,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Reservation::observe(ReservationObserver::class);
-        OtaSync::observe(OtaSyncObserver::class);
         GuestProfile::observe(GuestProfileObserver::class);
     }
 }
